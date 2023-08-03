5791d13156791e00560e16ac1b04c2c838aef7b3
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-08-03 18:58:24 +0530

    / endpoint updated

--- index.js
+++ index.js
@@ -11,21 +11,20 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-
+app.set('view engine', 'ejs');
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-}
-)
+    // Render the EJS template and send it as a response
+    res.render('index');
+});
+
 if (db()) {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`)
     }
     )
 }
-
-
 
 
 app.use('/api/v1/tasks', taskRouter)
